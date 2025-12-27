8ae2e602dc00aa8299022bbf5e41cd742fa5dfca
zero <zero@192.168.1.7>
2025-12-28 00:20:53 +0700

    fix github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { vitePluginManusRuntime } from "vite-plugin-manus-runtime";
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime()];
 
 export default defineConfig({
+   base: "/krita_web_app/", 
   plugins,
   resolve: {
     alias: {
